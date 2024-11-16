a91e537faf7b7a53a02626ea5882bdfe35084137
Md Christien <mdchristien12@gmail.com>
2024-11-16 01:06:35 +0100

    refactor: light up the computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,16 +10,9 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
+      <ambientLight intensity={2} />
+      <directionalLight position={[10, 10, 5]} intensity={2} />
+      <directionalLight position={[-10, 10, -5]} intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
